77a68fa2d6035411ffb3074096c1315a5e4fc908
ssd256 <zmffn2@gmail.com>
2021-12-08 23:34:26 +0900

    [2021.12.08] 2단계 정규표현식 고민중. '-'가 자꾸 빠짐

--- src/Level_1/New_ID.java
+++ src/Level_1/New_ID.java
@@ -35,7 +35,7 @@ class SetId {
 	     만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
 	7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
  */ 
-	 
+	  
 	 
 	public String checkReg(String str) {
 
